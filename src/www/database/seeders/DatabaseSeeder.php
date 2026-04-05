c354334a5e3139deccce22514a0ecf84825e7b68
shu-matsukubo <docha-sm29@outlook.jp>
2026-04-05 23:28:10 +0900

    シーだーを削除

--- src/www/database/seeders/DatabaseSeeder.php
+++ src/www/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,9 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            \Database\Seeders\Expenses\ExpenseCategoriesSeeder::class,
-            \Database\Seeders\Expenses\ExpensePaymentMethodsSeeder::class,
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
